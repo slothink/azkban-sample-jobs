4ee8bfc1a212d21fdd8e333e05f3d56da268cf6d
sai <sai@saiui-MacBook-Pro.local>
2017-07-03 11:31:40 +0900

    simple-java job 추가

---
+++ simple-java/src/main/java/world/sai/azkaban_samples/simple_java/SimpleJavaApplication.java
@@ -0,0 +1,8 @@
+package world.sai.azkaban_samples.simple_java;
+
+public class SimpleJavaApplication {
+
+    public static void main(String[] args) {
+        System.out.println("simple-job-ok");
+    }
+}
